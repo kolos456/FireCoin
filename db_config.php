36a2a411bb0f30b8542f34e213571f3fa403838a
kolos456 <98667914+kolos456@users.noreply.github.com>
2025-03-13 18:26:49 +0300

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,6 +1,20 @@
 <?php
-$servername = "mysql-firecoinbase.alwaysdata.net"; // Адрес сервера
-$db_username = "403942";     // Имя пользователя
-$db_password = "hugabos223";         // Пароль
-$dbname = "firecoinbase_bd"; // Имя базы данных
+// Параметры подключения к базе данных
+$servername = "mysql-firecoinbase.alwaysdata.net";  // Замените на актуальный хост
+$username = "403942";          // Имя пользователя базы данных
+$password = "hugabos223";          // Ваш пароль
+$dbname = "firecoinbase_bd";             // Имя базы данных
+
+// Создаем соединение
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверяем соединение
+if ($conn->connect_error) {
+    die("Ошибка подключения: " . $conn->connect_error);
+} else {
+    echo "Подключение успешно!";
+}
+
+// Закрытие соединения
+$conn->close();
 ?>
