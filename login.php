64587aabd416b4cc2ad2f96edcede9136e0b0a9f
kolos456 <98667914+kolos456@users.noreply.github.com>
2025-03-13 19:28:58 +0300

    Create login.php

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+header('Content-Type: application/json');
+
+// Подключаемся к базе данных
+$host = 'localhost';
+$user = 'root';
+$password = 'your_password';
+$dbname = 'firecoinbase_bd';
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(['success' => false, 'message' => 'Ошибка подключения к базе данных']));
+}
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (isset($data['username']) && isset($data['password'])) {
+    $username = $conn->real_escape_string($data['username']);
+    $password = $conn->real_escape_string($data['password']);
+
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false, 'message' => 'Неверный логин или пароль']);
+    }
+}
+
+$conn->close();
+?>
